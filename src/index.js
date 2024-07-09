63b55d3a82c13aad1b68563d87a361529e5169f9
doong-jo <sdong001@gmail.com>
2024-07-09 11:35:41 +0900

    Upgrade tailwind v3

--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
 const plugin = require('tailwindcss/plugin')
 
 const scrollbarHide = plugin(function ({ addUtilities }) {
-  addUtilities({
-    '.scrollbar-hide': {
-      /* IE and Edge */
-      '-ms-overflow-style': 'none',
+  addUtilities(
+    {
+      '.scrollbar-hide': {
+        /* IE and Edge */
+        '-ms-overflow-style': 'none',
 
-      /* Firefox */
-      'scrollbar-width': 'none',
+        /* Firefox */
+        'scrollbar-width': 'none',
 
-      /* Safari and Chrome */
-      '&::-webkit-scrollbar': {
-        display: 'none'
+        /* Safari and Chrome */
+        '&::-webkit-scrollbar': {
+          display: 'none'
+        }
+      },
+
+      '.scrollbar-default': {
+        /* IE and Edge */
+        '-ms-overflow-style': 'auto',
+
+        /* Firefox */
+        'scrollbar-width': 'auto',
+
+        /* Safari and Chrome */
+        '&::-webkit-scrollbar': {
+          display: 'block'
+        }
       }
     },
-    
-    '.scrollbar-default': {
-      /* IE and Edge */
-      '-ms-overflow-style': 'auto',
-
-      /* Firefox */
-      'scrollbar-width': 'auto',
-
-      /* Safari and Chrome */
-      '&::-webkit-scrollbar': {
-        display: 'block'
-      }
+    {
+      respectPrefix: true,
+      respectImportant: true
     }
-  }, ['responsive'])
+  )
 })
 
 module.exports = scrollbarHide
