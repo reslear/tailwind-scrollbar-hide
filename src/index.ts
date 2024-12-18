c765477ff818052ec2c9063712df42281a0b482c
reslear <reslear@gmail.com>
2024-12-18 19:21:03 +0100

    migrate to typescript

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import type { PluginCreator } from 'tailwindcss/types/config'
+
+/**
+ * Tailwind plugin for hide scrollbars, although the element can still be scrolled if the element's content overflows.
+ */
+const scrollbarHide: PluginCreator = ({ addUtilities }) =>
+  addUtilities({
+    '.scrollbar-hide': {
+      /* IE and Edge */
+      '-ms-overflow-style': 'none',
+
+      /* Firefox */
+      'scrollbar-width': 'none',
+
+      /* Safari and Chrome */
+      '&::-webkit-scrollbar': {
+        display: 'none'
+      }
+    },
+
+    '.scrollbar-default': {
+      /* IE and Edge */
+      '-ms-overflow-style': 'auto',
+
+      /* Firefox */
+      'scrollbar-width': 'auto',
+
+      /* Safari and Chrome */
+      '&::-webkit-scrollbar': {
+        display: 'block'
+      }
+    }
+  })
+
+// override type for v3/4 compatibility
+export = scrollbarHide as () => void
